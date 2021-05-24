852ca792d98e3c3f6690bd847c736a9516fa1fef
Kanwar Pal Singh <kanwarpalsingh@Kanwars-MacBook-Pro.local>
2021-05-24 11:05:48 -0400

    My first commit for printing output

---
+++ js/myjs.js
@@ -0,0 +1,36 @@
+// alert( "description of alerted_value: " + alerted_value );
+/*
+a + b
+a - b
+a * b
+a / b
+a % b
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a == b
+a != b
+a > b
+a < b
+!a && !c 
+!a || !c 
+*/
+
+
+// Task 1
+let a=3;
+let b=5;
+let c;
+
+alert ("Let a = " + a + ";" + "\nLet b = " + b + ";\nLet c\n-------\na + b = " + (a+b) + "\na - b = " + (a-b) + "\na * b = " + (a*b) + "\na / b = " + (a/b) + "\na % b = " + (a%b) + "\na += b = " + (a+=b) + "\na -= b =" + (a-=b) + "\na *= b = " + (a*=b) + "\na /= b = " + (a/=b) + "\na %= b = " + (a%=b) + "\na == b: " + (a==b) + "\na != b: " + (a!=b) + "\na > b: " + (a>b) + "\na < b: " + (a<b) + "\n!a && !c: " + (!a&&!c) + "\n!a || !c: " + (!a||!c));
+
+
+// Task 2
+let first_name="Kanwar Pal";
+let last_name="Singh";
+let email="sing1517@algonquinlive.com";
+let output;
+output = "My name is "+first_name+" "+last_name+". You can connect me at "+email+".";
+console.log(output);
